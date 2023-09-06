c6af40d56429b9c4f739a5d07f9c75174a53dab6
Leonard12138 <75907531+Leonard12138@users.noreply.github.com>
2023-09-06 15:23:07 +0800

    camera

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.os.Bundle
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +15,79 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.myapplication.R
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var camera: Camera
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var surfaceHolder: SurfaceHolder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        surfaceView = findViewById(R.id.cameraPreview)
+        surfaceHolder = surfaceView.holder
+        surfaceHolder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                // Handle surface changes, e.g., start the camera preview
+                startCameraPreview()
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                // Surface is created
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                // Surface is destroyed
+            }
+        })
+
+        // Check for camera permissions
+        if (checkCameraPermission()) {
+            // Initialize the camera here if permission is granted
+            initializeCamera()
+        } else {
+            // Request camera permissions
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun initializeCamera() {
+        try {
+            camera = Camera.open()
+            // Set the camera to the surface holder
+            camera.setPreviewDisplay(surfaceHolder)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun startCameraPreview() {
+        camera.startPreview()
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 101
     }
 }
 
